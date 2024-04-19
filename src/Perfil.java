76499ca565e4c23c4c74bd84186ab272a870fd5b
LeandroCastro2 <leandro2684566@outlook.com.br>
2024-04-19 19:36:14 -0300

    Subindo conteúdos para o github

--- src/Perfil.java
+++ src/Perfil.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Perfil {
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Leandro!");
+        System.out.println("Tudo bem?");
+
+    }
 }
